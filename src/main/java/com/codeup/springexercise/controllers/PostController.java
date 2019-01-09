42b33cf5caf37aa65aa596062357aa7f7de8a9f6
karen-rivas0410 <karen-rivas0410@gmail.com>
2019-01-09 12:17:28 -0600

    had to add responsebody and change last one to postmapping

--- src/main/java/com/codeup/springexercise/controllers/PostController.java
+++ src/main/java/com/codeup/springexercise/controllers/PostController.java
@@ -3,27 +3,32 @@ package com.codeup.springexercise.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
+    @ResponseBody
     public String index(){
         return "posts index page";
     }
 
     @GetMapping("/posts/{id}")
+    @ResponseBody
     public String view(@PathVariable long id) {
-        return "view an individual post" + id;
+        return "view an individual post with id: " + id;
     }
 
     @GetMapping("/posts/create")
+    @ResponseBody
     public String create(){
         return "view the form for creating a post";
     }
 
-    @GetMapping("/posts/create")
+    @PostMapping("/posts/")
+    @ResponseBody
     public String newPost() {
         return "create a new post";
     }
