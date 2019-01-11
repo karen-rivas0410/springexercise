8f9511dc50197a6cd3136d195b2b75fe514eaa77
karen-rivas0410 <karen-rivas0410@gmail.com>
2019-01-11 15:39:54 -0600

    have random num and guess added to html

--- src/main/java/com/codeup/springexercise/controllers/DiceController.java
+++ src/main/java/com/codeup/springexercise/controllers/DiceController.java
@@ -8,18 +8,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class DiceController {
 
-    private int num;  //need this to call variable in all//
+    private int randomNum;  //need this to call variable in all//
 
     @GetMapping("/roll-dice")
     public String rollDice() {
-        num = (int) Math.floor(Math.random() * 6 ) + 1;
+        randomNum = (int) Math.floor(Math.random() * 6 ) + 1;
         return "rollDice";
     }
 
     @GetMapping("roll-dice/{n}")
     public String rollDice(@PathVariable int n, Model model) {
         model.addAttribute("n", n);
-        model.addAttribute("num", num);
+        model.addAttribute("randomNum", randomNum);
         return "rollDice";
     }
 
