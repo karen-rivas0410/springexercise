94191040f00252737d144b92dbb70b2c713b8002
karen-rivas0410 <karen-rivas0410@gmail.com>
2019-01-14 10:51:38 -0600

    added post controller along with fixing partials

--- src/main/java/com/codeup/springexercise/controllers/PostController.java
+++ src/main/java/com/codeup/springexercise/controllers/PostController.java
@@ -1,18 +1,34 @@
 package com.codeup.springexercise.controllers;
 
+import com.codeup.springexercise.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
+
+    private List<Post> posts; //post object is created to have an array of posts//
 
     @GetMapping("/posts")
     @ResponseBody
     public String index(){
         return "posts index page";
+    }
+    @GetMapping("/posts")
+    public String index(Model model) {
+        posts = new ArrayList<>();
+        posts.add(new Post("New Post 1", "lorem impsum dolor set amit",1));
+        posts.add(new Post("New Post 2", "lorem impsum dolor set amit", 2));
+        posts.add(new Post("New Post 3", "lorem impsum dolor set amit", 3));
+        model.addAttribute("posts",posts);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
